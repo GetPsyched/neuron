12fbfa26d0f2c7ed7b13b354f63fe3f9b94d54f5
GetPsyched <priyanshutr@proton.me>
2023-10-10 16:00:31 +0530

    feat: basic cli

--- neuron-cli/src/main.rs
+++ neuron-cli/src/main.rs
@@ -1,7 +1,20 @@
-use neuron;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct NeuronCli {
+    /// Name of the person to greet
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    let x = 10;
-    let y = 20;
-    println!("Hello, world! {x} + {y} = {}", neuron::add(x, y));
+    let args = NeuronCli::parse();
+
+    for _ in 0..args.count {
+        println!("Hello, {}!", args.name)
+    }
 }
